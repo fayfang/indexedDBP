f0231276d3b5799a89bea25eecc57b9e28b9cf67
fayfang <gvs5527856@163.com>
2019-05-16 18:21:17 +0800

    add test

---
+++ test/db.test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var IndexDBP = require('../dist/indexDBP').default;
+var expect = chai.expect
+
+describe('test of db operation', function() {
+  it('open a database', async function() {
+    let mydb = new IndexDBP({name: 'testDB'})
+    await mydb.init();
+    expect(mydb.db.name).to.equal('testDB')
+  })
+})

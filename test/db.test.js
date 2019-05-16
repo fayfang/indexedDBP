b56e78ab1b82d8a4297778fb6c1498c8b8af0ca5
fayfang <gvs5527856@163.com>
2019-05-16 18:33:30 +0800

    test travis

--- test/db.test.js
+++ test/db.test.js
@@ -1,11 +1,11 @@
 var chai = require('chai');
 var IndexDBP = require('../dist/indexDBP').default;
-var expect = chai.expect
+var expect = chai.expect;
 
 describe('test of db operation', function() {
   it('open a database', async function() {
-    let mydb = new IndexDBP({name: 'testDB'})
+    let mydb = new IndexDBP({name: 'testDB'});
     await mydb.init();
-    expect(mydb.db.name).to.equal('testDB')
+    expect(mydb.db.name).to.equal('testDB');
   })
 })
